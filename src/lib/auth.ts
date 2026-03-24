d30a9b3954b78c4f44e8942cdcc462434a23b9f3
Arthur Santos <santos.santos86@hotmail.com>
2026-03-23 23:34:57 -0300

    fix

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,8 +3,7 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
 
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, {
-        provider: "postgresql",
-        
-    }),
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
 });
